c356b9322d5fedadbd837ce4dd54ea9ac91234d7
Alexmop <sergioalejandromorales@outlook.es>
2024-04-04 12:48:42 -0600

    Primeras clases curso JavaScript

---
+++ archivo.js
@@ -0,0 +1,58 @@
+// redondear a siguiente entero en decimal .5
+var precio = Math.round(399.53);
+document.write('precio redondeado: ', precio);
+document.write('<br>')
+
+// redondear sin importar decimal hacia arriba
+var precio = Math.ceil(299.9);
+document.write('precio redondeado: ', precio);
+document.write('<br>');
+
+// redondear sin importar decimal hacia arriba
+var precio = Math.floor(540.9);
+document.write('precio redondeado: ', precio);
+document.write('<br>');
+
+//calcular el seno de un angulo
+var seno = Math.sin(45);
+document.write('seno de 45: ', seno);
+document.write('<br>');
+
+//calcular el exponencial de un numero
+var expo = Math.exp(2);
+document.write('exponencial de 2: ', expo)
+document.write('<br>');
+
+//calcular el logaritmo de un numero
+var logaritmo = Math.log(10);
+document.write('logaritmo de 10: ', logaritmo)
+document.write('<br>');
+
+//valor absoluto de un numero
+var absoluto = Math.abs(-10);
+document.write('v absoluto de -10: ', absoluto);
+document.write('<br>');
+
+//calcular valor maximo de secuencia
+var maximo = Math.max(10,50,600,1,8,);
+document.write('mayor valor: ', maximo)
+document.write('<br>');
+
+//calcular valor minimo de secuencia
+var minimo = Math.min(10,50,600,1,8,);
+document.write('menor valor: ', minimo)
+document.write('<br>');
+
+//traer en pantalla valor aleatorio
+var aleatorio = Math.round(Math.random());
+document.write('valor aleatorio: ', aleatorio)
+document.write('<br>');
+
+//raiz cuadrada de un numero
+var valoe = Math.sqrt(81);
+document.write('raiz de 81: ', valor)
+document.write('<br>');
+
+//calcular el exponente de un numero
+var exponente = Math.pow(4,2);
+document.write('valor de 4 a la 2: ', exponente)
